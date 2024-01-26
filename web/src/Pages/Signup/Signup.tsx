9791ecad0fd627e6fc40797e627b8a17e4be85f9
bibekshhh <bibekrock2019@gmail.com>
2024-01-27 03:49:12 +0545

    [add]: signup page

--- web/src/Pages/Signup/Signup.tsx
+++ web/src/Pages/Signup/Signup.tsx
@@ -1,7 +1,91 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+// signup assets
+import SignupArt from "./signup-art.svg";
+import "./Signup.css";
+
+// arco-design components
+import { Spin, Typography } from "@arco-design/web-react";
+
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUserName] = useState("");
+  const [name, setFullName] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
   return (
     <>
-    this is signup page
+    <div className="signup-form-main-wrapper">
+        <div className="img-container">
+          <img src={SignupArt} alt="signup art" />
+        </div>
+        <div className="signup-form-container">
+          <div className="signup-form">
+            <Typography.Text 
+            type="primary"
+            className="my-3"
+            style={{fontSize: 22}}
+            bold
+            >
+              Explore further. Signup now.
+            </Typography.Text>
+
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setFullName(e.target.value)}
+              className="input-fullname"
+              placeholder="Full name"
+            />
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUserName(e.target.value)}
+              className="input-username"
+              placeholder="Username"
+            />
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="input-email"
+              placeholder="Phone number, user or email"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input-password"
+              placeholder="Password"
+            />
+
+            <Spin
+              delay={500}
+              loading={loading}
+              style={{ width: "100%", color: "white", marginRight: 5 }}
+            >
+              <button
+                style={{ width: "100%" }}
+                // onClick={(e) => signupHandle(e)}
+                type="submit"
+                className="submit-btn mb-3"
+              >
+                Sign Up
+              </button>
+            </Spin>
+          </div>
+          <div className="login-div">
+            <p>
+              Already have an account?  <Link style={{ textDecoration: "none", color: "black" }} to="/login">
+               Login
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- web/src/routes/PublicRoutes.tsx
+++ web/src/routes/PublicRoutes.tsx
@@ -1,10 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from '../Pages/Login/Login';
+import Signup from '../Pages/Signup/Signup';
 
 export const PublicRoutes = () => {
     return (
         <Routes>
             <Route path='login' element={<Login />} />
+            <Route path='signup' element={<Signup />} />
             <Route path='*' element={<Navigate to='/login' replace />} />
         </Routes>
     );
