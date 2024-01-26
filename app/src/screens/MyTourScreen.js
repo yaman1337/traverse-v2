da65bced78b12082da8de2bf49eabb3725f81dd6
SajjanKarn <karnaa787@gmail.com>
2024-01-26 21:59:17 +0545

    assets added and designed landing screen with contexts and screens

---
+++ app/src/screens/MyTourScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MyTourScreen = () => {
+  return (
+    <View>
+      <Text>MyTourScreen</Text>
+    </View>
+  );
+};
+
+export default MyTourScreen;
