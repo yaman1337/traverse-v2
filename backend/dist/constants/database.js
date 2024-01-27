6ec33dcb1b336d71ac6deca5aba994dcde4008da
Yaman Sarabariya <yaman.sarabariya@gmail.com>
2024-01-28 02:37:15 +0545

    remove build

--- backend/dist/constants/database.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.databaseConfig = void 0;
-exports.databaseConfig = {
-    databaseId: "traverse",
-    placesCollectionId: "places",
-    loyaltyCollectionId: "loyalty"
-};
--- backend/dist/routes/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRouter = void 0;
-var auth_route_1 = require("./auth.route");
-Object.defineProperty(exports, "authRouter", { enumerable: true, get: function () { return auth_route_1.authRouter; } });
