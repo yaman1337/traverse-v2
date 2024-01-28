d2364e00164af62aa9d9f2c39659bc768a9e74e4
SajjanKarn <karnaa787@gmail.com>
2024-01-28 06:12:41 +0545

    added reviews and minor changes in ui screen

--- app/src/screens/DestinationDetailScreen.js
+++ app/src/screens/DestinationDetailScreen.js
@@ -14,8 +14,28 @@ import UserAvatar from "react-native-user-avatar";
 
 import AppText from "../components/AppText";
 import colors from "../../config/colors";
+import { useState } from "react";
 
 const DestinationDetailScreen = () => {
+  const [reviews, setReviews] = useState([
+    {
+      id: 1,
+      name: "Saurav",
+      date: "12th May, 2021",
+      rating: 4.7,
+      review:
+        "Located in the Rudraprayag district of Uttarakhand, India, the Kedarnath Temple stands as a revered Hindu pilgrimage site and holds profound spiritual significance. Situated at an elevation of 3,583 meters (11,755 feet), amidst the majestic Garhwal Himalayas, this ancient temple is dedicated to Lord Shiva. Kedarnath Temple is one of the twelve Jyotirlingas, which are considered the most sacred abodes of Lord Shiva. The temple's history dates back over a thousand years and is believed to have been built by Adi Shankaracharya, the renowned philosopher-saint of India. The journey to Kedarnath Temple is not only a test of faith but also a breathtaking adventure. Pilgrims undertake a challenging trek of approximately 16 kilometers (10 miles) from Gaurikund, which involves traversing rugged terrain and steep slopes. The path is adorned with stunning natural beauty, including the Mandakini River, snow-capped peaks, and dense forests. Along the way, several smaller shrines and resting points provide solace and respite to the weary travelers. The temple itself is a magnificent stone structure, intricately adorned with ornate carvings and sculptures. The serene ambiance and the reverberating sound of sacred chants create a deeply spiritual atmosphere. Devotees offer prayers and seek blessings from Lord Shiva, who is believed to reside in Kedarnath in his form as the 'Lord of Kedar Khand.' Kedarnath Temple is not only a place of devotion but also a testament to human resilience and devotion.",
+    },
+    {
+      id: 2,
+      name: "Saurav",
+      date: "12th May, 2021",
+      rating: 4.7,
+      review:
+        "Located in the Rudraprayag district of Uttarakhand, India, the Kedarnath Temple stands as a revered Hindu pilgrimage site and holds profound spiritual significance. Situated at an elevation of 3,583 meters (11,755 feet), amidst the majestic Garhwal Himalayas, this ancient temple is dedicated to Lord Shiva. Kedarnath Temple is one of the twelve Jyotirlingas, which",
+    },
+  ]);
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.headerNavigationContainer}>
@@ -47,6 +67,12 @@ const DestinationDetailScreen = () => {
             }}
             style={styles.destinationImage}
           />
+        </View>
+        <View style={styles.verifiedBadgeContainer}>
+          <AntDesign name="checkcircle" size={24} color="green" />
+          <AppText variant="Medium" style={styles.verifiedBadgeText}>
+            Verified
+          </AppText>
         </View>
         <View style={styles.destinationInfoContainer}>
           <View style={styles.destinationInfo}>
@@ -126,6 +152,41 @@ const DestinationDetailScreen = () => {
             devotion.
           </AppText>
         </View>
+
+        <View style={styles.reviesContainer}>
+          <AppText variant="Medium" style={styles.aboutDestinationText}>
+            Reviews
+          </AppText>
+
+          {reviews.map((review) => (
+            <View style={styles.reviewCardContainer} key={review.id}>
+              <View style={styles.reviewCardHeader}>
+                <UserAvatar size={40} name={review.name} />
+                <View style={styles.reviewCardHeaderInfo}>
+                  <AppText variant="Medium" style={styles.reviewCardHeaderName}>
+                    {review.name}
+                  </AppText>
+                  <AppText
+                    variant="Light"
+                    style={styles.reviewCardHeaderPostedOn}
+                  >
+                    {review.date}
+                  </AppText>
+
+                  <View style={styles.ratingContainer}>
+                    <AirbnbRating
+                      count={5}
+                      defaultRating={review.rating}
+                      size={15}
+                      showRating={false}
+                      isDisabled={true}
+                    />
+                  </View>
+                </View>
+              </View>
+            </View>
+          ))}
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -156,7 +217,7 @@ const styles = new StyleSheet.create({
     justifyContent: "center",
   },
   headerTitle: {
-    fontSize: totalSize(2.3),
+    fontSize: totalSize(2),
   },
   headerRightContainer: {
     width: totalSize(5),
@@ -215,6 +276,20 @@ const styles = new StyleSheet.create({
   authorNameContainer: {
     marginLeft: width(2),
   },
+  verifiedBadgeContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: width(30),
+    borderRadius: width(2),
+    paddingHorizontal: width(3),
+    paddingVertical: height(1),
+    backgroundColor: colors.lightGray,
+  },
+  verifiedBadgeText: {
+    fontSize: totalSize(1.5),
+    color: "green",
+    marginLeft: width(2),
+  },
   destinationAboutContainer: {
     paddingHorizontal: width(5),
     paddingVertical: height(1),
@@ -248,6 +323,31 @@ const styles = new StyleSheet.create({
     fontSize: totalSize(1.7),
     color: colors.white,
     textAlign: "center",
+  },
+
+  reviesContainer: {
+    paddingHorizontal: width(5),
+    paddingVertical: height(1),
+  },
+  reviewCardContainer: {
+    backgroundColor: colors.lightGray,
+    borderRadius: width(2),
+    padding: width(5),
+    marginVertical: height(1),
+  },
+  reviewCardHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  reviewCardHeaderInfo: {
+    marginLeft: width(2),
+  },
+  reviewCardHeaderName: {
+    fontSize: totalSize(2),
+  },
+  reviewCardHeaderPostedOn: {
+    fontSize: totalSize(1.5),
+    color: colors.gray,
   },
 });
 
